bc31804a8d4620e5db06b44eafd954e65c7d8ab0
Moe Ghanem <moeghanem@utexas.edu>
2023-03-03 14:14:13 -0600

    fixed login

--- ScreenBreak/ScreenBreak/ScreenBreakApp.swift
+++ ScreenBreak/ScreenBreak/ScreenBreakApp.swift
@@ -6,15 +6,23 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct ScreenBreakApp: App {
-    let persistenceController = PersistenceController.shared
-
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            SignInView().navigationTitle("Sign In")
         }
     }
 }
