5d19d602ded290e7bce6b88e5a1313e9a53658d3
Christian Pichardo <ChristianP62200@gmail.com>
2023-04-02 19:26:18 -0500

    Top three integrated

---
+++ ScreenBreak/ReportExtension/TopAppsReport.swift
@@ -0,0 +1,8 @@
+//
+//  TopAppsReport.swift
+//  ReportExtension
+//
+//  Created by Christian Pichardo on 4/2/23.
+//
+
+import Foundation
---
+++ ScreenBreak/ReportExtension/TopThreeView.swift
@@ -0,0 +1,20 @@
+//
+//  TopThreeView.swift
+//  ReportExtension
+//
+//  Created by Christian Pichardo on 4/2/23.
+//
+
+import SwiftUI
+
+struct TopThreeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TopThreeView_Previews: PreviewProvider {
+    static var previews: some View {
+        TopThreeView()
+    }
+}
