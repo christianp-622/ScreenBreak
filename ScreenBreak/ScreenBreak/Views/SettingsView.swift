d7ed9d639899bc779f83f4f5a71bde70508d04cb
mya-mahaley <mya.mahaley@yahoo.com>
2023-04-03 14:06:44 -0500

    simple profile page setup

--- ScreenBreak/ScreenBreak/Views/SettingsView.swift
+++ ScreenBreak/ScreenBreak/Views/SettingsView.swift
@@ -11,19 +11,46 @@ import RiveRuntime
 struct SettingsView: View {
     var body: some View {
         NavigationView {
-            ZStack{
-                RiveViewModel(fileName: "shapes").view()
-                    .ignoresSafeArea()
-                    .blur(radius:30)
-                    .background(
-                        Image("Spline")
-                            .blur(radius:60)
-                            .offset(x:200, y:100)
-                    )
-                Text("Settings")
-            }.navigationTitle("Settings")
+            ScrollView {
+                ZStack{
+                    VStack(alignment:.leading){
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 25, style: .continuous)
+                                .fill(.ultraThickMaterial)
+                                .frame(height: 200)
+                                .shadow(radius: 10)
+                            HStack{
+                                VStack{
+                                    Image("Profile").resizable().scaledToFit().frame(width: 150, height: 150)
+                                    Text("Display Name").font(.subheadline)
+                                    
+                                }
+                                VStack{
+                                    Button("Change Profile Picture"){}.buttonStyle(.bordered)
+                                    Button("Change Display Name"){}.buttonStyle(.bordered)
+                                }
+                            }.padding()
+                        }.padding()
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 25, style: .continuous)
+                                .fill(.ultraThickMaterial)
+                                .frame(height: 600)
+                                .shadow(radius: 10)
+                            VStack(alignment: .center){
+                                Text("Friend Requests").font(.title2)
+                                Divider()
+                                VStack(alignment: .center){
+                                    Text("No Current Requests").font(.title3)
+                                }.padding()
+                                Spacer()
+                            }.padding()
+                        }.padding()
+                        
+                    }
+                }.navigationTitle("Settings")
+            }
+            .navigationViewStyle(.stack)
         }
-        .navigationViewStyle(.stack)
     }
 }
 
