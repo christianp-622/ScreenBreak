e4101cf0400efcffdaa717b9f5346b98ce56ac40
Moe Ghanem <moeghanem@utexas.edu>
2023-04-09 17:19:09 -0500

    pickups chart view created

---
+++ ScreenBreak/ReportExtension/PickupsChartView.swift
@@ -0,0 +1,23 @@
+//
+//  PickupsChartView.swift
+//  ReportExtension
+//
+//  Created by Moe Ghanem on 4/9/23.
+//
+
+import SwiftUI
+import SwiftUICharts
+import DeviceActivity
+import Charts
+
+struct PickupsChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PickupsChartView_Previews: PreviewProvider {
+    static var previews: some View {
+        PickupsChartView()
+    }
+}
